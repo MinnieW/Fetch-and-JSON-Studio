b2a6234925ed3be1f7ff0dd218f2b6f7208b30b4
Minnie Washington <minniewashington@Minnies-MacBook-Pro.local>
2022-08-11 20:20:40 -0500

    first two bonuses complete

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ window.addEventListener("load", function () {
     fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function (response) {
         response.json().then(function (json) {
             let container = document.getElementById("container");
+            json.sort((a,b) => b.hoursInSpace - a.hoursInSpace)
             for (astronaut of json) {
                 container.innerHTML +=
                 `<div class="astronaut">
@@ -10,12 +11,15 @@ window.addEventListener("load", function () {
                         <h3>${astronaut.firstName} ${astronaut.lastName}</h3>
                         <ul>
                             <li>Hours in space: ${astronaut.hoursInSpace}</li>
-                            <li>Active: ${astronaut.active}</li>
+                            <li id= "${astronaut.id}">Active: ${astronaut.active}</li>
                             <li>Skills: ${astronaut.skills.join(", ")}</li>
                         </ul>
                     </div>
                     <img class="avatar" src=${astronaut.picture}>
             </div>`
+            if(astronaut.active === true){
+                document.getElementById(astronaut.id).style.color= "green"
+            }
             }
             //console.log(json)
         })
